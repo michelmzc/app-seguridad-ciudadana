b3382e447de33b7fdf33896d17b2c0be5bafce1d
michelmzc <michelmzco@gmail.com>
2025-03-05 15:46:14 -0300

    contenedor basico raiz funcionando

--- App.tsx
+++ App.tsx
@@ -1,35 +1,55 @@
-import React, {useState} from "react";
-import {View, StyleSheet} from "react-native";
+import React, {ReactNode, useState} from "react";
+import { View, StatusBar, StyleSheet, Platform} from "react-native";
+import { SafeAreaProvider, SafeAreaView   } from 'react-native-safe-area-context';
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
+interface StandardContainerProps{
+  children: ReactNode;
+}
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1, // Ocupa toda la pantalla pero respeta márgenes seguros
+  },
   container: {
-    ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+    flex: 1, // Permite que el contenido crezca dentro del área segura
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0, // Evita solapamiento en Android
   },
   map: {
     ...StyleSheet.absoluteFillObject,
   },
  });
 
+const StandardContainer: React.FC<StandardContainerProps> = ({ children }) => {
+  return(
+          <SafeAreaView style={ [styles.safeArea] }>
+            <View style={styles.container}>
+              { children } 
+            </View>
+          </SafeAreaView>
+  )
+}
+
+
+const Map = () => {
+  return(
+    <MapView
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        region={{
+          latitude:  -40.5742,
+          longitude: -73.1336,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      >
+    </MapView>
+  )
+}
 const App = () => {
   return (
-    <View style={styles.container}>
-    <MapView
-      provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-      style={styles.map}
-      region={{
-        latitude:  -40.5742,
-        longitude: -73.1336,
-        latitudeDelta: 0.015,
-        longitudeDelta: 0.0121,
-      }}
-    >
-    </MapView>
-  </View>
+     <StandardContainer>
+        <Map></Map>
+     </StandardContainer>
   );
 };
 
