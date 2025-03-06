0f89a6212e52bcc09c88e2b30063ce5d47257549
michelmzc <michelmzco@gmail.com>
2025-03-06 16:26:16 -0300

    funcionando la geolocalización

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import {StyleSheet} from "react-native";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import React, {useState, useEffect} from "react";
+import {View, PermissionsAndroid, Platform, StyleSheet} from "react-native";
+import MapView, {Marker} from "react-native-maps";
+import Geolocation from "@react-native-community/geolocation";
 
 const styles = StyleSheet.create({
   map: {
@@ -8,18 +9,65 @@ const styles = StyleSheet.create({
   },
  });
 
+function getInitialState(){
+  return {
+    region: {
+      latitude:  -40.5742,
+      longitude: -73.1336,
+      latitudeDelta: 0.015,
+      longitudeDelta: 0.0121,
+    }
+  }
+}
+
+
 const Map = () => {
+    const [location, setLocation] = useState<{ latitude: number; longitude: number } | null>(null);
+  
+    useEffect(() => {
+      const requestLocationPermission = async () => {
+        if (Platform.OS === "android"){
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+          );
+          if (granted !== PermissionsAndroid.RESULTS.GRANTED){
+            console.log("Permiso de ubicación denegado");
+            return;
+          }
+        }
+      
+
+        Geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+            setLocation({latitude, longitude});
+            console.log(position);
+          },
+          (error) => console.log("Error al obtener la ubicación:", error),
+          { enableHighAccuracy: true, timeout:15000, maximumAge:10000 }
+        );
+      };
+      requestLocationPermission();
+    }, []);
+
+
     return(
       <MapView
-          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+          provider="google"
           style={styles.map}
+          googleMapId="c43c633a2f9311f8" // solo funciona en android
+          showsUserLocation={true}
+          showsMyLocationButton={true}
+          rotateEnabled={false}
+          loadingEnabled={true}
           region={{
-            latitude:  -40.5742,
-            longitude: -73.1336,
+            latitude: location?.latitude || -40.573852935025464,
+            longitude: location?.longitude || -73.1358395458682,
             latitudeDelta: 0.015,
             longitudeDelta: 0.0121,
           }}
         >
+          {location && <Marker coordinate={location} title="Tu ubicación" />}
       </MapView>
     )
   }
