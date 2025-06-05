72b676d0c17dd49a94ebc05904e173e618c30367
michelmzc <michelmzco@gmail.com>
2025-06-04 23:02:34 -0400

    integración basica de Firebase funcionando

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 
-import React, {ReactNode} from "react";
+import React, {ReactNode, useEffect} from "react";
 import { View, StatusBar, StyleSheet, Platform} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import AppNavigator from "./navigation/StackNavigator";
 import { AuthProvider } from "./api/auth/AuthContext";
+
+import messaging from '@react-native-firebase/messaging';
+import { Alert } from 'react-native';
 
 interface StandardContainerProps{
   children: ReactNode;
@@ -34,6 +37,36 @@ const StandardContainer: React.FC<StandardContainerProps> = ({ children }) => {
 
 
 const App = () => {
+  useEffect(() => {
+    const setupFCM = async () => {
+      const authStatus = await messaging().requestPermission();
+      const enabled = 
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED || 
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+      if (enabled) {
+        const token = await messaging().getToken();
+        console.log('FCM Token:', token);
+
+        fetch('http://backend-seguridad-ciudadana.onrender.com:3000/fcm/register', {
+          method: 'POST',
+          headers: {'Contet-Type':'application/json'},
+          body: JSON.stringify({token})
+        });
+      } else {
+        Alert.alert('Permiso para notificaciones denegado');
+      }
+    };
+    setupFCM();
+  }, []);
+
+  useEffect(() => {
+    const unsuscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('Nueva notificación', JSON.stringify(remoteMessage.notification))
+    });
+    return unsuscribe;
+  });
+
   return (
     <AuthProvider>
       <StandardContainer>
